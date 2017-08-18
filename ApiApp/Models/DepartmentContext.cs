a3a9e511534ed64819d8c66147840f5feb068754
Fedyukov <msh3000@yandex.ru>
2017-08-19 01:17:37 +0500

    Добавьте файлы проекта.

---
+++ ApiApp/Models/DepartmentContext.cs
@@ -0,0 +1,9 @@
+﻿using System.Data.Entity;
+
+namespace WebApplication1.Models
+{
+    public class DepartmentContext : DbContext
+    {
+        public DbSet<Department> Departments { get; set; }
+    }
+}
---
+++ ApiApp/Models/DepartmentDbInitializer.cs
@@ -0,0 +1,16 @@
+﻿using System.Data.Entity;
+
+namespace WebApplication1.Models
+{
+    public class DepartmentDbInitializer : CreateDatabaseIfNotExists<DepartmentContext>
+    {
+        protected override void Seed(DepartmentContext db)
+        {
+            db.Departments.Add(new Department { Title = "IT Dep" });
+            db.Departments.Add(new Department { Title = "Marketing" });
+            db.Departments.Add(new Department { Title = "RND" });
+
+            base.Seed(db);
+        }
+    }
+}
