c4b39ddc39db1d4704b9e425e926761f214acb1c
Fedyukov <msh3000@yandex.ru>
2017-08-22 14:34:43 +0500

    New upload

---
+++ WebApplication1/Models/DepartmentDbInitializer.cs
@@ -0,0 +1,24 @@
+﻿using System.Data.Entity;
+
+namespace WebApplication1.Models
+{
+    public class DepartmentDbInitializer : DropCreateDatabaseAlways<DepartmentContext>
+    {
+        protected override void Seed(DepartmentContext db)
+        {
+            db.Departments.Add(new Department { Title = "Finance" });
+            db.Departments.Add(new Department { Title = "Procurement" });
+            db.Departments.Add(new Department { Title = "Legal & Contracts" });
+            db.Departments.Add(new Department { Title = "Factory Management" });
+            db.Departments.Add(new Department { Title = "Design" });
+            db.Departments.Add(new Department { Title = "R & D" });
+            db.Departments.Add(new Department { Title = "International Commercial" });
+            db.Departments.Add(new Department { Title = "Local Suppliers" });
+            db.Departments.Add(new Department { Title = "Local Product Sales" });
+            db.Departments.Add(new Department { Title = "Overseas Product Sales" });
+            db.Departments.Add(new Department { Title = "Marketing and Public Relations" });
+
+            base.Seed(db);
+        }
+    }
+}
---
+++ WebApplication1/Models/UserDbInitializer.cs
@@ -0,0 +1,23 @@
+﻿using System.Data.Entity;
+
+namespace WebApplication1.Models
+{
+    public class UserDbInitializer : DropCreateDatabaseAlways<UserContext>
+    {
+        protected override void Seed(UserContext db)
+        {
+            db.Users.Add(new User { UserName = "FearLeSS", DepartmentId = 10});
+            db.Users.Add(new User { UserName = "Slug-em-dog", DepartmentId = 5});
+            db.Users.Add(new User { UserName = "CodeExia", DepartmentId = 9});
+            db.Users.Add(new User { UserName = "Danqqqqq", DepartmentId = 7});
+            db.Users.Add(new User { UserName = "Flotsams54", DepartmentId = 1});
+            db.Users.Add(new User { UserName = "RZR", DepartmentId = 4});
+            db.Users.Add(new User { UserName = "Toxic-oxide", DepartmentId = 6});
+            db.Users.Add(new User { UserName = "DeathDancer", DepartmentId = 9});
+            db.Users.Add(new User { UserName = "Sw00sh", DepartmentId = 3});
+            db.Users.Add(new User {UserName = "LyRz", DepartmentId = 11});
+
+            base.Seed(db);
+        }
+    }
+}
